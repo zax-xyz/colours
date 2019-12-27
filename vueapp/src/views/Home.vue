c15e53f11514891f4042df8fc97305c42fed5263
Zaxutic <zaxuti@gmail.com>
2019-12-27 21:25:13 +1100

    Fix missing spaces

--- vueapp/src/views/Home.vue
+++ vueapp/src/views/Home.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
   div
     p
-      | Set your
-      router-link(to="/channels") channels
+      | Set your 
+      router-link(to="/channels") channels 
       | before starting the script!
 
     ButtonGet(
