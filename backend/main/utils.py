35d93253750edb7b42e1e20385189457370cedcd
Zaxutic <zaxutic@gmail.com>
2019-12-28 23:03:43 +1100

    Add function to get n evenly spaced colours

--- backend/main/utils.py
+++ backend/main/utils.py
@@ -1,4 +1,5 @@
 import re
+import colorsys
 
 
 def is_valid_colour(colour: str):
@@ -11,3 +12,10 @@ def is_valid_colour(colour: str):
     if colour in preset or re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', colour):
         return True
     return False
+
+
+def get_colours(num):
+    diff = 360 // num
+    rgbs = [colorsys.hls_to_rgb(h / 360, 0.5, 1) for h in range(0, 360, diff)]
+    hexes = ['#' + ''.join('%02X' % round(i * 255) for i in r) for r in rgbs]
+    return hexes
