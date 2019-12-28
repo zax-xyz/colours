771007bd49c761649dc53736efd45895328086df
Zaxutic <zaxuti@gmail.com>
2019-12-28 21:26:00 +1100

    Add views to admin page

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -3,7 +3,21 @@ from django.contrib import admin
 from .models import CeleryTask, Channel, Message, User
 
 # Register your models here.
+class ChannelInline(admin.TabularInline):
+    model = Channel.users.through
+
+
+class ChannelAdmin(admin.ModelAdmin):
+    model = Channel
+    filter_horizontal = ('users',)
+
+
+class UserAdmin(admin.ModelAdmin):
+    exclude = ('password',)
+    inlines = [ChannelInline]
+
+
 admin.site.register(CeleryTask)
-admin.site.register(Channel)
+admin.site.register(Channel, ChannelAdmin)
 admin.site.register(Message)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
